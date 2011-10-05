222c91436438fd7cde11ac494ed848ec57df3c0f
James Stuckey Weber <jamesnw@gmail.com>
2011-10-05 16:46:45 -0400

    Better organization of lists of words

--- generator.php
+++ generator.php
@@ -1,8 +1,10 @@
 <?php
 //Set up vars
-$words = array("Pittsburgh Experiment","Anabaptist","Martyr's Mirror","Menno Simons","Dirk Willems","Conrad Grebel","chow chow","bonnebrugge","John Paul Lederach","congregations","coverings","budget","pastor","Mennonite Central Committee","dove","colony","Venture Club","MYF","JYF","Goshen College","Eastern Mennonite University","Bluffton","Bethel","Hesston","The Corinthian Plan","Peace and Justice","Ulrich Zwingli","Felix Manz","adult baptism","nonresistance","Sermon on the Mount","in the name of the Father and of the Son and of the Holy Spirit","communal","fellowship meal","fellowship","community","simplicity","peace","conflict studies","service","missional","martyrs","MCC","AMBS","footwashing","Politics of Jesus","Old Mennonite Church","General Conference","ethnic Mennonite","Mennonite Meccas","Lancaster","head coverings","the Mennonite game","potluck","community","GC","EMU","brothers and sisters in Christ","heilsgeschichte","Beachy Amish","conservative conference","More with Less","green bean casserole","frugality","sola scriptura","Ten Thousand Villages","VS","MDS","cape dress","homegrown","Gethsemane","Schleitheim Confession","Felix Manz","January 21, 1525","Ulrich Zwingli","George Blaurock","borsht","tongue screw","conscientious objector","Asbund");
-$phrases = array("ate zweibach","let's delegate","let's come to agreement","decision by consensus","healing the world","peace by peace","who are the Mennonites","healing and hope","joining together","joyfully following Jesus","bridges to the cross","bringing forth firstfruits","share with each other","pass the peace","a kiss of brotherly love","longing for a greater experience","opt for believer's baptism","make disciples","baptizing them","teach all nations","in the world, but not of it","the third way","servant leadership","the Anabaptist Vision","the Silent in the land","gelassenheit","in the world but not of it","More with Less","compassionate peacemaking","singing hymns","devout servitude","don't own a TV","my 7 roommates","my Amish cousins","what we are called to do in the face of","make a committee");
-$fillers = array("and","'twas","was","now","or","not","is","isn't","to","at","our");
+
+
+$words = file('lists/words.txt');
+$phrases = file('lists/phrases.txt');
+$fillers = file('lists/fillers.txt');
 
 if(ISSET($_GET['p'])){
 	$paras = $_GET['p'];}
