43db65bf2d5e1c6688538b141ee3f50667431618
James Stuckey Weber <jamesnw@gmail.com>
2011-10-08 19:24:12 -0400

    Add base for multiple sentence structures, and add 2 new sentence structures

--- generator.php
+++ generator.php
@@ -22,21 +22,69 @@ for($i=1;$i<=$paras;$i++) {
 		$sentence_per_para = rand($sentence_min,$sentence_max);
 		echo '<p>';
 		for($j=1; $j<=$sentence_per_para;$j++) {
-			//generate random numbers
-			$words1rn = rand(0,sizeof($words)-1);
-			while (($words2rn = rand(0,sizeof($words)-1)) == $words1rn){};
-			$phrase1rn = rand(0,sizeof($phrases)-1);
-			while (($phrase2rn = rand(0,sizeof($phrases)-1)) == $phrase1rn){};
-			$filler1rn = rand(0,sizeof($fillers)-1);
-			//generate words
-			$word1 = rtrim($words[$words1rn]);
-			$word2 = rtrim($words[$words2rn]);
-			$phrase1 = ucfirst(rtrim($phrases[$phrase1rn]));
-			$phrase2 = rtrim($phrases[$phrase2rn]);
-			$filler1 = rtrim($fillers[$filler1rn]);
-			$sentence = $phrase1.' '.$word1.' '.$filler1.' '.$phrase2.' '.$word2.'. ';
-			echo $sentence;
+			$structure = rand(1,3);
+			switch($structure) {
+				case 1: //pwfpw
+					//generate random numbers
+					$words1rn = rand(0,sizeof($words)-1);
+					while (($words2rn = rand(0,sizeof($words)-1)) == $words1rn){};
+					$phrase1rn = rand(0,sizeof($phrases)-1);
+					while (($phrase2rn = rand(0,sizeof($phrases)-1)) == $phrase1rn){};
+					$filler1rn = rand(0,sizeof($fillers)-1);
+					//generate words
+					$word1 = rtrim($words[$words1rn]);
+					$word2 = rtrim($words[$words2rn]);
+					$phrase1 = ucfirst(rtrim($phrases[$phrase1rn]));
+					$phrase2 = rtrim($phrases[$phrase2rn]);
+					$filler1 = rtrim($fillers[$filler1rn]);
+					$sentence = $phrase1.' '.$word1.' '.$filler1.' '.$phrase2.' '.$word2.'. ';
+					echo $sentence;
+					break;
+				case 2: //pfw
+					//generate random numbers
+					$words1rn = rand(0,sizeof($words)-1);
+					$phrase1rn = rand(0,sizeof($phrases)-1);
+					$filler1rn = rand(0,sizeof($fillers)-1);
+					//generate words
+					$word1 = rtrim($words[$words1rn]);
+					$phrase1 = ucfirst(rtrim($phrases[$phrase1rn]));
+					$filler1 = rtrim($fillers[$filler1rn]);
+					$sentence = $phrase1.' '.$filler1.' '.$word1.'. ';
+					echo $sentence;	
+					break;
+				case 3: //pwfpw,pfw
+				
+					//generate random numbers
+					$words1rn = rand(0,sizeof($words)-1);
+					
+					while (($words2rn = rand(0,sizeof($words)-1)) == $words1rn){};
+					while (($words3rn = rand(0,sizeof($words)-1)) == $words2rn){};
+					$phrase1rn = rand(0,sizeof($phrases)-1);
+					
+					while (($phrase2rn = rand(0,sizeof($phrases)-1)) == $phrase1rn){};
+					while (($phrase3rn = rand(0,sizeof($phrases)-1)) == $phrase2rn){};
+					$filler1rn = rand(0,sizeof($fillers)-1);
+					
+					while (($filler2rn = rand(0,sizeof($fillers)-1)) == $filler1rn){};
+					
+					//generate words
+					$word1 = rtrim($words[$words1rn]);
+					$word2 = rtrim($words[$words2rn]);
+					$word3 = rtrim($words[$words3rn]);
+					$phrase1 = ucfirst(rtrim($phrases[$phrase1rn]));
+					$phrase2 = rtrim($phrases[$phrase2rn]);
+					$phrase3 = rtrim($phrases[$phrase3rn]);
+					$filler1 = rtrim($fillers[$filler1rn]);
+					$filler2 = rtrim($fillers[$filler2rn]);
+					$sentence = $phrase1.' '.$word1.' '.$filler1.' '.$phrase2.' '.$word2.', '.$phrase3.' '.$filler2.' '.$word3.'. ';
+					echo $sentence;
+					break;
+				default:
+					return "Broken";
+			}
 		}
 		echo '</p>';
 	}
+	
+
 ?>
