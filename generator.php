577c8d15416c5d2bf54c67ea3e7aa1c106ef94e2
Marcos Wright Kuhns <webmaster@kuhnsfam.com>
2011-10-20 09:30:47 -0700

    Use echoRandSentence to DRY things up
    Also enhance echoRandSentence to handle basic punctuation.

--- generator.php
+++ generator.php
@@ -56,88 +56,34 @@ function echoRandSentence($structure, $words, $phrases, $fillers) {
 	$sentence = array();
 	foreach ($structure as $entry) {
 		switch($entry) {
-			case 'w': $sentence[] = rtrim($words[array_pop($rand_words)]); break;
-			case 'p': $sentence[] = rtrim($phrases[array_pop($rand_phrases)]); break;
-			case 'f': $sentence[] = rtrim($fillers[array_pop($rand_fillers)]); break;
+			case 'w': $sentence[] = ' '.rtrim($words[array_pop($rand_words)]); break;
+			case 'p': $sentence[] = ' '.rtrim($phrases[array_pop($rand_phrases)]); break;
+			case 'f': $sentence[] = ' '.rtrim($fillers[array_pop($rand_fillers)]); break;
+			// Handle extra punctuation in the $structure
+			default:  $sentence[] = $entry;
 		}
 	}
 
 	// Print out a nice English-formatted sentence.
-	$sentence[0] = ucfirst($sentence[0]);
-	echo "<p>";
-	echo implode(' ', $sentence).'. ';
-	echo "</p>\n";
+	$sentence[0] = ucfirst(trim($sentence[0]));
+	echo implode('', $sentence).'. ';
 }
 
 //generate paragraphs
 for($i=1;$i<=$paras;$i++) {
+		echo "<p>";
 		//generate sentences
 		$sentence_per_para = rand($sentence_min,$sentence_max);
-		echo '<p>';
 		for($j=1; $j<=$sentence_per_para;$j++) {
 			$structure = rand(1,3);
 			switch($structure) {
-				case 1: //pwfpw
-					//generate random numbers
-					$words1rn = rand(0,sizeof($words)-1);
-					while (($words2rn = rand(0,sizeof($words)-1)) == $words1rn){};
-					$phrase1rn = rand(0,sizeof($phrases)-1);
-					while (($phrase2rn = rand(0,sizeof($phrases)-1)) == $phrase1rn){};
-					$filler1rn = rand(0,sizeof($fillers)-1);
-					//generate words
-					$word1 = rtrim($words[$words1rn]);
-					$word2 = rtrim($words[$words2rn]);
-					$phrase1 = ucfirst(rtrim($phrases[$phrase1rn]));
-					$phrase2 = rtrim($phrases[$phrase2rn]);
-					$filler1 = rtrim($fillers[$filler1rn]);
-					$sentence = $phrase1.' '.$word1.' '.$filler1.' '.$phrase2.' '.$word2.'. ';
-					echo $sentence;
-					break;
-				case 2: //pfw
-					//generate random numbers
-					$words1rn = rand(0,sizeof($words)-1);
-					$phrase1rn = rand(0,sizeof($phrases)-1);
-					$filler1rn = rand(0,sizeof($fillers)-1);
-					//generate words
-					$word1 = rtrim($words[$words1rn]);
-					$phrase1 = ucfirst(rtrim($phrases[$phrase1rn]));
-					$filler1 = rtrim($fillers[$filler1rn]);
-					$sentence = $phrase1.' '.$filler1.' '.$word1.'. ';
-					echo $sentence;	
-					break;
-				case 3: //pwfpw,pfw
-				
-					//generate random numbers
-					$words1rn = rand(0,sizeof($words)-1);
-					
-					while (($words2rn = rand(0,sizeof($words)-1)) == $words1rn){};
-					while (($words3rn = rand(0,sizeof($words)-1)) == $words2rn){};
-					$phrase1rn = rand(0,sizeof($phrases)-1);
-					
-					while (($phrase2rn = rand(0,sizeof($phrases)-1)) == $phrase1rn){};
-					while (($phrase3rn = rand(0,sizeof($phrases)-1)) == $phrase2rn){};
-					$filler1rn = rand(0,sizeof($fillers)-1);
-					
-					while (($filler2rn = rand(0,sizeof($fillers)-1)) == $filler1rn){};
-					
-					//generate words
-					$word1 = rtrim($words[$words1rn]);
-					$word2 = rtrim($words[$words2rn]);
-					$word3 = rtrim($words[$words3rn]);
-					$phrase1 = ucfirst(rtrim($phrases[$phrase1rn]));
-					$phrase2 = rtrim($phrases[$phrase2rn]);
-					$phrase3 = rtrim($phrases[$phrase3rn]);
-					$filler1 = rtrim($fillers[$filler1rn]);
-					$filler2 = rtrim($fillers[$filler2rn]);
-					$sentence = $phrase1.' '.$word1.' '.$filler1.' '.$phrase2.' '.$word2.', '.$phrase3.' '.$filler2.' '.$word3.'. ';
-					echo $sentence;
-					break;
-				default:
-					return "Broken";
+				case 1: $structure = 'pwfpw'; break;
+				case 2: $structure = 'pfw'; break;
+				case 3: $structure = 'pwfpw,pfw'; break;
 			}
+			echoRandSentence($structure, $words, $phrases, $fillers);
 		}
-		echo '</p>';
+		echo "</p>\n";
 	}
-	
 
 ?>
